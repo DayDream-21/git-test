4cc8e1a9eb23808d6ebc9849e475beae65c64907
DayDream-21 <vova13158@gmail.com>
2021-04-07 23:54:51 +0300

    online commit test

--- src/GitClass2.java
+++ src/GitClass2.java
@@ -2,6 +2,7 @@ public class GitClass2 {
     public static void main(String[] args) {
         int a = 10;
         int b = 55;
-        System.out.println(a + Integer.toString(b));
+        int c = 45;
+        System.out.println(a + Integer.toString(b) + c);
     }
 }
